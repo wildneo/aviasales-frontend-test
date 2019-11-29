9fc30cbeac171f0b90c114787a9f0137d36096cc
Roman Oskolkov <wild_neo@mail.ru>
2019-11-29 17:40:54 +0500

    add temporary reduce

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,62 @@
 import { handleActions } from 'redux-actions';
 import { combineReducers } from 'redux';
+import { groupBy } from 'lodash';
 import * as actions from '../actions';
 
-export const fetchSearchState = handleActions({
+export const union = (newObj, oldObj) => {
+  const keys = Object.keys(newObj);
+  const unionObj = keys.map((key) => (
+    // obj2[key] ? obj2[key].concat(obj1[key]) : obj1[key]
+    oldObj[key]
+      ? [...oldObj[key], ...newObj[key]]
+      : newObj[key]
+  ));
+
+  return unionObj;
+};
+
+export const maxStops = (segments) => {
+  const allStops = segments.map(({ stops }) => stops.length);
+
+  return Math.max(...allStops);
+};
+
+export const getDuration = (segments) => (
+  segments.reduce((acc, { duration }) => (acc + duration), 0)
+);
+
+
+
+const fetchSearchState = handleActions({
   [actions.fetchSearchIdRequest]: () => 'requested',
   [actions.fetchSearchIdFailure]: () => 'failed',
   [actions.fetchSearchIdSuccess]: () => 'finished',
 }, 'none');
 
-export const fetchTicketsState = handleActions({
+const fetchTicketsState = handleActions({
   [actions.fetchTicketsRequest]: () => 'requested',
   [actions.fetchTicketsFailure]: () => 'failed',
   [actions.fetchTicketsSuccess]: () => 'finished',
 }, 'none');
 
-export const tickets = handleActions({
-  [actions.fetchTicketsSuccess]: (state, { payload: { tickets } }) => ([
-    ...state,
-    ...tickets,
-  ]),
-}, []);
+const tickets = handleActions({
+  [actions.fetchTicketsSuccess]: (state, { payload: { tickets } }) => {
+    // const test = tickets.filter((t) => maxStops(t.segments) < 3);
+    // const all = state.all.concat(tickets);
+    const all = [...state.all, ...tickets];
+    const group = groupBy(tickets, (t) => maxStops(t.segments));
+    const byStops = union(group, state.byStops);
+
+    return { all, byStops };
+  }
+}, { all: [], byStops: {} });
+
+// export const stops = handleActions({
+//   [actions.fetchTicketsSuccess]: (state, { payload: { tickets } }) => ([
+//     ...state,
+//     ...tickets,
+//   ]),
+// }, []);
 
 export default combineReducers({
   fetchSearchState,
