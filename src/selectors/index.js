57d06375f1109055bdf298c7e3bc6fe7e7df23e5
Roman Oskolkov <wild_neo@mail.ru>
2019-12-05 12:20:15 +0500

    add filter selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,25 +1,24 @@
 import { createSelector } from 'reselect';
 import { sortBy } from 'lodash';
-import { getTotalDuration } from '../utils';
-
-// const getTasksById = (state) => state.tasks.byId;
-// const getTaskIds = (state) => state.tasks.allIds;
-// const getCurrentFilterName = (state) => state.tasks.currentFilterName;
-
-// export const tasksSelector = createSelector(
-//   [getTasksById, getTaskIds],
-//   (byId, allIds) => allIds.map((id) => byId[id]),
-// );
-
-// export const filteredTasksSelector = createSelector(
-//   [tasksSelector, getCurrentFilterName],
-//   (tasks, filterName) => (filterName === 'all' ? tasks : tasks.filter((t) => t.state === filterName)),
-// );
+import { getTotalDuration, declinationHelper } from '../utils';
 
 export const getAllTickets = (state) => state.tickets.all;
 export const getTicketsByStops = (state) => state.tickets.byStops;
-export const getStopFilters = (state) => state.stopFilters;
+export const getAllFiltersState = (state) => state.stopFilters.setAll;
+export const getStopFilters = (state) => state.stopFilters.byStops;
 export const getSorts = (state) => state.filters.sortingState;
+
+export const possibleFiltersSelector = createSelector(
+  [getStopFilters],
+  (byStops) => (Object.keys(byStops)
+    .map((value) => {
+      const label = declinationHelper(value);
+      const isChecked = byStops[value];
+
+      return { value, label, isChecked };
+    })
+  ),
+);
 
 export const filteredTicketsSelector = createSelector(
   [getAllTickets, getTicketsByStops, getStopFilters],
