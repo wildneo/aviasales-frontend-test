ed3c4d5b9e9c88813c7a0d7e3f6ff77137404b91
Roman Oskolkov <wild_neo@mail.ru>
2019-12-23 16:25:31 +0500

    add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { setConfiguration } from 'react-grid-system';
-import reducers from './reducers';
 import { initSearch } from './actions';
-import App from './components/App';
+import reducers from './reducers';
+import { setConfiguration } from 'react-grid-system';
 import 'semantic-ui-css/semantic.min.css';
+import App from './components/App';
 import './index.css';
 
 setConfiguration({ defaultScreenClass: 'md', gutterWidth: 20 });
 
-/* eslint-disable no-underscore-dangle */
-const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
-const devtoolMiddleware = ext && ext();
-/* eslint-enable */
-
 const store = createStore(
   reducers,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
-    devtoolMiddleware,
   ),
 );
 
