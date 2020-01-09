2f39db42dc5346a5d92fef7c240b41122b3798b3
Roman Oskolkov <wild_neo@mail.ru>
2020-01-09 16:23:47 +0500

    Updates

---
+++ precache-manifest.fa7af0da5e52528b701064977f6016d4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821d149bd32941f3fb02d54ed43d6ad6",
+    "url": "/aviasales-frontend-test/index.html"
+  },
+  {
+    "revision": "296541cf249a11c1e61c",
+    "url": "/aviasales-frontend-test/static/css/2.cb9555c7.chunk.css"
+  },
+  {
+    "revision": "5f4ec17b862648ddec96",
+    "url": "/aviasales-frontend-test/static/css/main.49cd1e9c.chunk.css"
+  },
+  {
+    "revision": "296541cf249a11c1e61c",
+    "url": "/aviasales-frontend-test/static/js/2.9cf11752.chunk.js"
+  },
+  {
+    "revision": "fd375cc9336e94d38f86f79bf9c4f2ed",
+    "url": "/aviasales-frontend-test/static/js/2.9cf11752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f4ec17b862648ddec96",
+    "url": "/aviasales-frontend-test/static/js/main.660812cb.chunk.js"
+  },
+  {
+    "revision": "1062be74b19a13979345",
+    "url": "/aviasales-frontend-test/static/js/runtime-main.c2194c73.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/aviasales-frontend-test/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/aviasales-frontend-test/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/aviasales-frontend-test/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/aviasales-frontend-test/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/aviasales-frontend-test/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/aviasales-frontend-test/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/aviasales-frontend-test/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/aviasales-frontend-test/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/aviasales-frontend-test/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/aviasales-frontend-test/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/aviasales-frontend-test/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales-frontend-test/static/media/logo.0c089234.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/aviasales-frontend-test/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/aviasales-frontend-test/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/aviasales-frontend-test/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/aviasales-frontend-test/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/aviasales-frontend-test/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aviasales-frontend-test/precache-manifest.fa7af0da5e52528b701064977f6016d4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aviasales-frontend-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
